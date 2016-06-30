5de62053313bbe4b651d26a1073d53d4f3c327d7
zvezd13 <thrashzvezd@gmail.com>
2016-06-30 20:42:48 +0300

    update Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate{
 	public static void main(String[] arg){
 		System.out.println("Calculate");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("sum: " + sum);		
 	}
 }
